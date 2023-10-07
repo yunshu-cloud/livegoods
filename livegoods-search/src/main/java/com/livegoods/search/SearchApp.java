4d82e00cf370f45ae05e51dedd71e4b49bfec918
yunshu-cloud <w2099266050@gmail.com>
2023-10-07 12:22:36 +0800

    模块搭建

---
+++ livegoods-search/src/main/java/com/livegoods/search/SearchApp.java
@@ -0,0 +1,12 @@
+package com.livegoods.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApp.class,args);
+    }
+}
