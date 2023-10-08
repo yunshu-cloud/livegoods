50811715e6ca966dd92ddb1d01bba66895c976eb
yunshu-cloud <w2099266050@gmail.com>
2023-10-08 13:47:11 +0800

    redis模板

--- livegoods-cache-redis/src/main/java/com/livegoods/cache/config/RedisCacheConfiguration.java
+++ livegoods-cache-redis/src/main/java/com/livegoods/cache/config/RedisCacheConfiguration.java
@@ -1,4 +1,21 @@
 package com.livegoods.cache.config;
 
-public class RedisCacheConfiguration {
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * 定义一个父类，用户提供模板配置
+ */
+public abstract class RedisCacheConfiguration {
+    protected RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+
 }
